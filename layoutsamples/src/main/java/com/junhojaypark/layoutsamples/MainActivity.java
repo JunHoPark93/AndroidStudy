31386db74fbd4ba81808447d70feff27347cdc5f
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-04 22:55:59 +0900

    Create layout sample module

---
+++ layoutsamples/src/main/java/com/junhojaypark/layoutsamples/MainActivity.java
@@ -0,0 +1,32 @@
+package com.junhojaypark.layoutsamples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private long initTime;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        // 뒤로 가기 버튼을 눌렀을 때
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            if(System.currentTimeMillis() - initTime < 3000) {
+                finish();
+            }
+            Toast.makeText(this, "종료 하려면 한 번 더 누르세요", Toast.LENGTH_SHORT).show();
+            initTime = System.currentTimeMillis();
+
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
